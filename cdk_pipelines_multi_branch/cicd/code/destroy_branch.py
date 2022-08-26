ae0f1f09233348e419390819e6bb37257d47f498
Iris Kraja <39408109+iriskraja77@users.noreply.github.com>
2022-08-26 17:40:27 +0200

    Update destroy_branch.py
    Update codebuild image to the latest (standard:6.0)

--- cdk_pipelines_multi_branch/cicd/code/destroy_branch.py
+++ cdk_pipelines_multi_branch/cicd/code/destroy_branch.py
@@ -54,7 +54,7 @@ def handler(event, context):
                 },
                 environment={
                     'type': 'LINUX_CONTAINER',
-                    'image': 'aws/codebuild/standard:4.0',
+                    'image': 'aws/codebuild/standard:6.0',
                     'computeType': 'BUILD_GENERAL1_SMALL'
                 },
                 serviceRole=role_arn
